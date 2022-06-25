1f8a96b198938677b77ab3774be01d06b4cdd5af
Daniel Ojeyomi <73875730+danielswift10@users.noreply.github.com>
2022-06-25 04:24:20 -0700

    Add files via upload

---
+++ zuri-js-intro/index.js
@@ -0,0 +1,5 @@
+const myName = "Daniel Ojeyomi";  //Declaring variable myName
+const myHeight = "180cm"; //Declaring variable myHeight
+const myCountry = "Nigeria"; //Declaring variable myCountry
+
+alert("My name is " + myName + "\nMy Height is " + myHeight + "\nI am from " + myCountry); //Calling the alert function
