8dc31b601362dc20c68f40c976288ef7e11b2f91
Daniel Ojeyomi <73875730+danielswift10@users.noreply.github.com>
2022-06-25 08:15:06 -0700

    Delete zuri-js-intro directory

--- zuri-js-intro/index.js
+++
@@ -1,5 +0,0 @@
-const myName = "Daniel Ojeyomi";  //Declaring variable myName
-const myHeight = "180cm"; //Declaring variable myHeight
-const myCountry = "Nigeria"; //Declaring variable myCountry
-
-alert("My name is " + myName + "\nMy Height is " + myHeight + "\nI am from " + myCountry); //Calling the alert function
